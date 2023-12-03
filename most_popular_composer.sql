3bd15f7c10f7509fdbdf0d5faefc749a08088bf3
zhoum929 <89593559+zhoum929@users.noreply.github.com>
2023-12-03 11:23:08 -0500

    Update most_popular_composer.sql

--- most_popular_composer.sql
+++ most_popular_composer.sql
@@ -4,6 +4,6 @@ SELECT com FROM
 		GROUP BY a.composer) AS d 
 WHERE d.ct = (SELECT MAX(ctx) from 
 	(SELECT COUNT(ax.composer) AS ctx FROM Piece AS ax, Performance_piece AS bx, Performance AS cx 
-		WHERE  ax.piece_id=bx.piece_id AND bx.concert_id=cx.concert_id AND  cx.date_time>='1985-01-01' AND c.date_time<='2025-01-01'
+		WHERE  ax.piece_id=bx.piece_id AND bx.concert_id=cx.concert_id AND  cx.date_time>='1985-01-01' AND cx.date_time<='2025-01-01'
 		GROUP BY ax.composer) AS dx)
 		
