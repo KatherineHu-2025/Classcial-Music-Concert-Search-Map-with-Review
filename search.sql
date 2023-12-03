5739fc3ef270809c016f9a3526db9055469b0e30
zhoum929 <89593559+zhoum929@users.noreply.github.com>
2023-12-03 11:24:47 -0500

    Update search.sql

--- search.sql
+++ search.sql
@@ -1,4 +1,4 @@
-SELECT UNIQUE a.title, a.concert_id FROM Concert AS a, Piece AS b, Venue AS c, Performance as d, Performance_piece as e
+SELECT DISTINCT a.title, a.concert_id FROM Concert AS a, Piece AS b, Venue AS c, Performance as d, Performance_piece as e
 WHERE a.concert_id=d.concert_id AND 
 	  a.concert_id=e.concert_id AND
 	  b.piece_id=e.piece_id AND
