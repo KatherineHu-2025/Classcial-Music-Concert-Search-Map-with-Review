529bb8937851f1c47a41131ac993aa6f38bd43e1
KatherineHu-2025 <112400355+KatherineHu-2025@users.noreply.github.com>
2023-12-14 21:59:50 -0500

    remove repetitive insert comment

--- server.js
+++ server.js
@@ -43,20 +43,6 @@ app.get('/search_comment/:perform_id', (req, res) => {
     });
 });
 
-app.post('/insert_comment', (req, res) => {
-    let {content, rate, perform_id} = req.body;
-    console.log(content);
-    // Insert into Comment table
-    let commentQuery = `INSERT INTO Comment (content, rate) VALUES (?, ?);`;
-    db.query(commentQuery, [content, rate], (error, commentResults) => {
-        if (error) {
-            res.status(500).send('Error inserting comment');
-            console.error(error);
-            return;
-        }
-        res.json(results);
-    });
-});
 
 app.post('/insert_comment', (req, res) => {
     let {content, rate, perform_id} = req.body;
